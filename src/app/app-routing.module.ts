933f4922560f87edfdd3bd0f1abcaa3633788b01
Benoit CROZEMARIE <benoit.crozemarie@gmail.com>
2023-03-08 20:20:42 +0100

    ADD: path route preview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,18 @@ import { BorneListComponent } from './borne-list/borne-list.component';
 
 const routes: Routes = [
   {path: '', component: BorneListComponent},
+  /*
+  {path: '/login', component: *LoginComponent*}, //Page de connexion
+  {path: '/register', component: *RegisterComponent*}, //Page d'inscription
+  {path: '/stations/:cityName', component: *BorneListComponent*}, //Liste des stations après une recherche
+  {path: '/stations/:idStation', component: *BorneDetailComponent*}, // Details station
+  {path: '/profil/:idUser', component: *Component*}, REDIRECT TO'/profil/{id}/details'
+  {path: '/profil/:idUser/mydetails', component: *Component*}, // Details user
+  {path: '/profil/:idUser/mystation', component: *Component*},
+  {path: '/profil/:idUser/myreservation', component: *Component*},
+  {path: '/coktail/:idCoktail', component: *Component*},
+  */
+  {path: '**', component: BorneListComponent}
 ];
 
 @NgModule({
--- src/app/borne-list/borne-list.component.ts
+++ src/app/borne-list/borne-list.component.ts
@@ -11,7 +11,6 @@ export class BorneListComponent {
   stations : IStation[] | null = null;
 
   constructor(private apiStationService : ApiStationService) {
-    
   }
 
   ngOnInit(){

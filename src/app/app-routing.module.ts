7b5fe0d84c31bf98ffaddac360c227b6e0a03215
Benoit CROZEMARIE <benoit.crozemarie@gmail.com>
2023-03-08 12:49:26 +0100

    fix: rouing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BorneListComponent } from './borne-list/borne-list.component';
 
-const routes: Routes = [{path: '', component: BorneListComponent}];
+const routes: Routes = [
+  {path: '', component: BorneListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

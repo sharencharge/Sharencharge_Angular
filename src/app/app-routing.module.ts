35eccaf238d544d02bc3bc911924e63da2b58d62
Benoit CROZEMARIE <benoit.crozemarie@gmail.com>
2023-03-14 14:39:14 +0100

    ADD : authorization id user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessGuard } from 'src/config/access-guard';
 import { BorneDetailsComponent } from './borne-details/borne-details.component';
 import { BorneListComponent } from './borne-list/borne-list.component';
+import { FormInscriptionComponent } from './form-inscription/form-inscription.component';
 import { LoginComponent } from './login/login.component';
 import { ProfilBorneComponent } from './profil/profil-borne/profil-borne.component';
 import { ProfilDetailComponent } from './profil/profil-detail/profil-detail.component';
 import { ProfilReservationComponent } from './profil/profil-reservation/profil-reservation.component';
-import { FormInscriptionComponent } from './form-inscription/form-inscription.component';
 import { ZShaChaHistComponent } from './z-sha-cha-hist/z-sha-cha-hist.component';
 
 const routes: Routes = [
@@ -15,9 +16,9 @@ const routes: Routes = [
   {path: 'register', component: FormInscriptionComponent},
   {path: 'shacha', component: ZShaChaHistComponent},
   {path: 'stations/:idStation', component: BorneDetailsComponent}, // Details station
-  {path: 'profil/:idUser/mydetails', component: ProfilDetailComponent}, // Details user
-  {path: 'profil/:idUser/mystation', component: ProfilBorneComponent},
-  {path: 'profil/:idUser/myreservation', component: ProfilReservationComponent},
+  {path: 'profil/mydetails', component: ProfilDetailComponent, data:{requiresLogin: true}, canActivate: [ AccessGuard ]}, // Details user
+  {path: 'profil/mystation', component: ProfilBorneComponent},
+  {path: 'profil/myreservation', component: ProfilReservationComponent},
   /*
   {path: 'register', component: *RegisterComponent*}, //Page d'inscription
   {path: 'stations/:cityName', component: *BorneListComponent*}, //Liste des stations après une recherche
--- src/models/i-token.ts
+++ src/models/i-token.ts
@@ -1,3 +1,4 @@
 export interface IToken {
+    id : number;
     accessToken: string;
   }

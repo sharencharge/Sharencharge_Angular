71156892eadf472b53b51a7c93aeef9e6c29fd6d
Benoit CROZEMARIE <benoit.crozemarie@gmail.com>
2023-03-14 14:40:08 +0100

    Add : profils components

--- src/app/profil/profil-borne/profil-borne.component.ts
+++ src/app/profil/profil-borne/profil-borne.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IUser } from 'src/models/i-user';
+import { UserService } from 'src/service/user.service';
 
 @Component({
   selector: 'app-profil-borne',
   templateUrl: './profil-borne.component.html',
   styleUrls: ['./profil-borne.component.scss']
 })
-export class ProfilBorneComponent {
+export class ProfilBorneComponent implements OnInit{
+
+  user : IUser | undefined;
+
+  constructor(private route : ActivatedRoute,
+              private userService : UserService) {
+
+  }
+
+  ngOnInit(): void {
+    console.log(localStorage.getItem("id"));
+    
+    this.findUser(Number(localStorage.getItem("id")));
+  }
   
+  findUser(idUSer :number) {
+    this.userService.findUserById(idUSer).subscribe(res => {
+      this.user = res;
+    });
+  }
 }
